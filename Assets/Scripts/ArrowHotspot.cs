87f2d785a61aaf466e5909cb959de8ab3e1a8d93
Teaganbrown44 <tbrow038@uottawa.ca>
2025-11-02 14:22:39 -0500

    added the arrowhotspots

---
+++ Assets/Scripts/ArrowHotspot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ArrowHotspot : MonoBehaviour
+{
+    [Tooltip("Invoked when this arrow is clicked")]
+    public UnityEvent onClicked;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    onClicked.Invoke(); // your teammate can hook into this
+                }
+            }
+        }
+    }
+}

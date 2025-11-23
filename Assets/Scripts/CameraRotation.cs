172f6b2615c9c68e26d24e4e1b597f8a5e1eb643
Rolol0ko <ronan.keown613@gmail.com>
2025-11-22 22:38:23 -0500

    Click + drag Camera

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,34 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    public Vector2 turn;
-
-    public float sensitivity = .5f;
-
-    public Vector3 deltaMove;
-
-    public float speed = 1;
-
-    // stops/starts Update (called when menus open/close)
-    public void SetMovementActive(bool active)
-    {
-        enabled = active;                         
-        Cursor.lockState = active ? CursorLockMode.Locked : CursorLockMode.None;
-        Cursor.visible = !active;
-    }
+    private float X;
+    private float Y;
 
     void Update()
     {
-
-        turn.x += Input.GetAxis("Mouse X") * sensitivity;
-
-        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
-
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
-
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
     }
-
 }
